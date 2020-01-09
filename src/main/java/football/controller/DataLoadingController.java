8e8b9e556e4eb7cdbc1d1a10d449c67754eb0bdd
cnaant <cnaant73@gmail.com>
2020-01-09 14:45:02 +0200

    Basic Version

---
+++ src/main/java/football/controller/DataLoadingController.java
@@ -0,0 +1,24 @@
+package football.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import football.input.data.mine.DataMiner;
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+public class DataLoadingController {
+	private DataMiner dataMiner;
+	
+	@PostMapping("/football/loadData/{seasonId}/all")
+	@ResponseStatus(HttpStatus.CREATED)
+	public void loadAll(@PathVariable int seasonId, @RequestParam String token) {
+		dataMiner.readMultipleSeasons(seasonId, token);
+		
+	}
+}
