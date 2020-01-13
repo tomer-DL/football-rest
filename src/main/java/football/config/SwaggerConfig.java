3390060f63edbf5f2fad247d3f51e477e8203a7e
cnaant <cnaant73@gmail.com>
2020-01-13 21:50:16 +0200

    Swagger Configuration

---
+++ src/main/java/football/config/SwaggerConfig.java
@@ -0,0 +1,45 @@
+package football.config;
+
+import java.util.ArrayList;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.google.common.base.Predicates;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.Tag;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(Predicates.not(PathSelectors.regex("/error.*")))
+				.build()
+				.pathMapping("/")
+				.apiInfo(metaData())
+				.tags(new Tag("Data Loading Controller", "Controller for loading data from rapidapi"),
+						new Tag("Fixture Controller", "Controller for getting fixtures"),
+						new Tag("League Controller", "Controller for getting leagues"),
+						new Tag("Meta Data Controller", "Controller for showing the DB status"),
+						new Tag("Standings Controller", "Controller for showing league standings"),
+						new Tag("Team Controller", "Controller for getting teams"));
+	}
+	
+	private ApiInfo metaData() {
+		Contact contact = new Contact("Tomer Cnaan", "https://github.com/tomer-DL/football-rest", "cnaant@yahoo.com");
+		return new ApiInfo("Football API", "Basic Football Rest API", "1.0", "", contact, "", "", new ArrayList<>());
+	}
+	
+}
