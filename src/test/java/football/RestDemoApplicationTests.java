b1ff5778892b8a02876d580809be5302fea2781c
cnaant <cnaant73@gmail.com>
2020-01-16 23:20:19 +0200

    back to the basic version

--- src/test/java/football/RestDemoApplicationTests.java
+++ src/test/java/football/RestDemoApplicationTests.java
@@ -1,13 +1,13 @@
 package football;
 
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
+
 @SpringBootTest
 class RestDemoApplicationTests {
 
+	@Test
 	void contextLoads() {
 		
 	}
